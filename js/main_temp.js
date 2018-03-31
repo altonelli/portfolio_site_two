4adbeabcf8d3947dda7be33b119222821cecb306
altonelli <altonelli14@gmail.com>
2018-03-31 00:19:09 -0700

    js

---
+++ js/main_temp.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    imgStr = 'linear-gradient(160deg, rgba(150, 150, 150, 0.7), rgba(10, 10, 10, 0.9)), url("../img/headshot_edited_dull.jpg")';
+    $('.bg').css("background-image", imgStr);
+});
