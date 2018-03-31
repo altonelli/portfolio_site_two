73c9cb10b6bf8d794fa8dca6dc845329b1547c05
altonelli <altonelli14@gmail.com>
2018-03-31 00:29:07 -0700

    hope this works

--- js/main_temp.js
+++ js/main_temp.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(window).load(function() {
-    imgStr = 'linear-gradient(160deg, rgba(150, 150, 150, 0.7), rgba(10, 10, 10, 0.9)), url("../img/headshot_edited_dull.jpg")';
-    $('.bg').css("background-image", imgStr);
+    imgStr = 'linear-gradient(160deg, rgba(150, 150, 150, 0.7), rgba(10, 10, 10, 0.9)), url("img/headshot_edited_dull.jpg")';
+    $('.bg').css({"background-image": imgStr});
   });
 });
